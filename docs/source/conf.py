3c9db1e99cb9331e0d45ac17e278fac203e22482
William Mandai <wm@gitbench.com>
2018-11-16 16:27:17 +0300

    Removed source links

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -189,5 +189,6 @@ intersphinx_mapping = {'https://docs.python.org/': None}
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+html_show_sourcelink = False
 def setup(app):
     app.add_stylesheet('css/custom.css')
