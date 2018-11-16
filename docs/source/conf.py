d27dcdd81230831e31b349602a3eae5bca3fcbd7
William Mandai <wm@gitbench.com>
2018-11-16 15:54:18 +0300

    Attempt to fix css

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -189,4 +189,5 @@ intersphinx_mapping = {'https://docs.python.org/': None}
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
-html_css_files = ['custom.css']
+html_style = 'custom.css'
+html_css_files = ['css/custom.css']
