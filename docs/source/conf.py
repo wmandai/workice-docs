a1d093e8d3beabdccb2bf1b0d0523d68bc70f994
William Mandai <wm@gitbench.com>
2019-02-01 18:57:01 +0300

    Disabled sitemap generator

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
-    'sphinx_sitemap'
+    # 'sphinx_sitemap'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -193,8 +193,8 @@ intersphinx_mapping = {'https://docs.python.org/': None}
 todo_include_todos = True
 html_show_sourcelink = False
 html_show_sphinx = False
-html_baseurl = 'https://docs.workice.com/en/latest'
-site_url = 'https://docs.workice.com/en/latest'
+html_baseurl = 'https://docs.workice.com/en/latest/'
+site_url = 'https://docs.workice.com/en/latest/'
 html_favicon = 'images/favicon-32x32.png'
 def setup(app):
     app.add_stylesheet('css/custom.css')
